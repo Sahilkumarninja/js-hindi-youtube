193a096cfeafefb82c3d998da4a4ee780ba12800
Sahil Kumar Verma <132731225+Sahilkumarninja@users.noreply.github.com>
2025-04-08 14:56:11 +0530

    js_hindi

---
+++ 05_string.js
@@ -0,0 +1,33 @@
+const name = "sahil"
+const repocount = 50
+
+// console.log(name + repoCount + "Value");
+
+console.log(`Hello my name is ${name} and my repo count is$ {repocount}`);
+const gameName = new String('Sahilth')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameNmae.indexof('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-1, 4)
+console.log(anotherString);
+
+
+const newStringOne = " Sahil "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://sahil.com/sahil%20verma"
+
+console.log(url.replace('%', '-'))
+
+(url.includes('sahil'))
+
